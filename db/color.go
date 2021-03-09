032d3d058a9e4bb5e5c98bb88414308e8030c886
aikon001 <aikon01.ml@gmail.com>
2021-03-09 21:27:51 +0100

    bla

--- db/color.go
+++ db/color.go
@@ -16,7 +16,7 @@ func (db Database) GetAllColors() (*models.ColorList, error) {
 	}
 	for rows.Next() {
 		var color models.Color
-		err := rows.Scan(&color.ID, &color.Name, &color.hexadecimal, &color.R, &color.G, &color.B, &color.CreatedAt)
+		err := rows.Scan(&color.ID, &color.Name, &color.Hexadecimal, &color.R, &color.G, &color.B)
 		if err != nil {
 			return list, err
 		}
@@ -32,16 +32,16 @@ func (db Database) AddColor(color *models.Color) error {
 
 	query := `INSERT INTO colors (name, hexadecimal,R,G,B) VALUES ($1,$2,$3,$4,$5) RETURNING id, created_at`
 
-	if len(color.hexadecimal) != 0 {
-		byt, _ := hex.DecodeString(color.hexadecimal)
-		err := db.Conn.QueryRow(query, color.Name, color.hexadecimal, byt[0], byt[1], byt[2]).Scan(&id, &createdAt)
+	if len(color.Hexadecimal) != 0 {
+		byt, _ := hex.DecodeString(color.Hexadecimal)
+		err := db.Conn.QueryRow(query, color.Name, color.Hexadecimal, byt[0], byt[1], byt[2]).Scan(&id)
 		if err != nil {
 			return err
 		}
 
 	} else if unsafe.Sizeof(color.R)+unsafe.Sizeof(color.G)+unsafe.Sizeof(color.B) != 0 {
 		rgb := (*[3]byte)(unsafe.Pointer(&color.R))[:]
-		err := db.Conn.QueryRow(query, color.Name, color.hexadecimal, rgb[2], rgb[1], rgb[0]).Scan(&id, &createdAt)
+		err := db.Conn.QueryRow(query, color.Name, color.Hexadecimal, rgb[2], rgb[1], rgb[0]).Scan(&id)
 		if err != nil {
 			return err
 		}
@@ -59,12 +59,22 @@ func (db Database) GetItemById(colorId int) (models.Color, error) {
 	color := models.Color{}
 	query := `SELECT * FROM colors WHERE id = $1;`
 	row := db.Conn.QueryRow(query, colorId)
-	err := row.Scan(&color.ID, &color.Name, &color.hexadecimal, &color.R, &color.G, &color.B, &color.CreatedAt)
+	err := row.Scan(&color.ID, &color.Name, &color.Hexadecimal, &color.R, &color.G, &color.B)
 	return color, err
 }
 
-func (db Database) DeleteItem(colorId int) error {
+func (db Database) DeleteColor(colorId int) error {
 	query := `DELETE FROM items WHERE id = $1;`
 	_, err := db.Conn.Exec(query, colorId)
 	return err
+}
+
+func (db Database) UpdateColor(colorId int, colorData models.Color) (models.Color, error) {
+	color := models.Color{}
+	query := `UPDATE colors SET name=$1, hexadecimal=$2, R=$3, G=$4, B=$5 WHERE id=$6 RETURNING id, name, hexadecimal,R,G,B, created_at;`
+	err := db.Conn.QueryRow(query, colorData.Name, colorData.Hexadecimal, colorData.R, colorData.G, colorData.B, colorData.ID).Scan(&color.ID, &color.Name, &color.Description, &color.CreatedAt)
+	if err != nil {
+		return color, err
+	}
+	return color, nil
 }
